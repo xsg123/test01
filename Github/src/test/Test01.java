b0f59fad69f93e063c902c7f9ca9d59c451d39f5
Administrator <dada@xx.com>
2017-08-21 17:16:43 +0800

    第二次提交

--- Github/src/test/Test01.java
+++ Github/src/test/Test01.java
@@ -1,9 +1,16 @@
 package test;
 
 public class Test01 {
+	//int x=10;
+	//int y=20;
 	
 	public static void main(String[] args){
+		add(10,20 );
+	}
+	
+	public static void add(int x,int y){
 		
+		int sum=x+y;
 	}
 
 }
